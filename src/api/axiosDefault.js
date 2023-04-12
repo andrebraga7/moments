06ec386cac7e8670bd1fb69c3802a4ec9ceecbea
andrebraga7 <andrestrevisan@gmail.com>
2023-04-12 13:21:37 +0000

    Add the logic to post and catch errors from the api

--- src/api/axiosDefault.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-axios.defaults.baseURL = 'https://drf-api-andrebraga7.herokuapp.com/';
-axios.defaults.headers.post['ContentType'] = 'multipart/form-data';
-axios.defaults.withCredentials = true;
